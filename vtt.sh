518c5aa2e96c7ddb87021c7a12a09ef4a5944b5e
Garza <garza@netrun.net>
2021-08-03 09:30:22 -0700

    Update vtt.sh

--- vtt.sh
+++ vtt.sh
@@ -1,18 +1,11 @@
-#wget https://aka.ms/dependencyagentlinux
-#sh InstallDependencyAgent-Linux64.bin -s
-
-##install and apply the CEF collector
 apt-get update
 sudo apt install -y libssl-dev
 sudo apt install unzip
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
 cd $HOME
-sudo mkdir foundryvtt
 sudo mkdir /foundrydata
-#sudo mount /dev/sdc1 /foundrydata
 wget -O foundryvtt.zip $1
 sudo unzip foundryvtt.zip
-#node resources/app/main.js --dataPath=$HOME/foundrydata
 sudo npm install pm2@latest -gls
 sudo pm2 start resources/app/main.js -- --dataPath=/foundrydata
