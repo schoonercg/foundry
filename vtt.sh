efb7dde673cabb4d4fba17060016c073f817439a
Garza <garza@netrun.net>
2021-08-03 11:19:02 -0700

    Update vtt.sh

--- vtt.sh
+++ vtt.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 apt-get update
-sudo apt install -y libssl-dev unzip nodejs
+sudo apt-get install -y libssl-dev unzip nodejs
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 cd /home/adminuser
 sudo mkdir /foundrydata
+sudo mkdir /home/adminuser/foundryvtt
 sudo wget -O /home/adminuser/foundryvtt.zip $1
-sudo unzip /home/adminuser/foundryvtt.zip
+sudo unzip /home/adminuser/foundryvtt.zip -d /home/adminuser/foundryvtt
 sudo npm install pm2@latest -gls
 sudo pm2 start /home/adminuser/foundryvtt/resources/app/main.js -- --dataPath=/foundrydata
